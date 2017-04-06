fc4fdc1e4fc8f94f3e1aacbd2a30257d77163e56
alemjimer <alemjimer@gmx.de>
2017-04-06 22:37:35 +0200

    Update DeploymentPipeline.groovy

--- DeploymentPipeline.groovy
+++ DeploymentPipeline.groovy
@@ -1,6 +1,12 @@
 
 node (){
   
+  sh 'mkdir hallo'
+  stage ('checkout') {
+    dir('hallo') {
+      git credentialsId: 'f2a8f3f0-db0f-4de5-b181-ab7e790dd42a', url: 'https://github.com/alemjimer/jenkins-pipeline.git'
+    }
+  }
   stage('Build') {
     echo 'Hallo'
     echo 'Halllllllo'
